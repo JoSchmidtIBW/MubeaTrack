ebf01cc1e7a82a51c4956bade8dcc79bff35f725
John <flnc85@hotmail.de>
2023-02-19 02:31:54 +0100

    test eslint, and learn about dotenv

--- app.mjs
+++ app.mjs
@@ -1,7 +1,26 @@
+import morgan from 'morgan';
 import express from 'express';
 
 const app = express();
 
+// Middleware
+
+// Development logging
+console.log('wiso undefinded??? process.env.NODE_ENV: ' + process.env.NODE_ENV); // wiso ist dieser undefinded? bei server.mjs ist morgan zuoberst und erst dann app
+if (process.env.NODE_ENV === 'development') {
+  // wenn undefined oder development... :)
+  app.use(morgan('dev')); // wenn nicht geht, hat man kein logger!
+}
+
+app.use(express.json()); //für post (daten von client zu bekommen), muss json sein
+
+// my middleware
+app.use((req, res, next) => {
+  console.log('Hello from the middleware :)');
+  next();
+});
+
+// Routes
 app.get('/', (req, res) => {
   res.status(200).send('hello from server from app.mjs');
 });
